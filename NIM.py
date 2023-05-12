4c0ed5613d65558ac00a2c1014dd805b36c98f4f
minhtrang160199 <ntmtrang160199@gmail.com>
2023-05-12 16:35:39 +0100

    Commit all

---
+++ NIM.py
@@ -0,0 +1,124 @@
+# Load packages
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load data 
+df = pd.read_excel('/Users/mimi/Documents/Tech_data.xlsx', sheet_name='Data', usecols=('Name', 'Year','Earning_asset',
+                    'Net_revenue', 'NPL', 'CAR', 'Credit_insti_lending', 'Credit_insti_allowance','Cust_lending','Cust_allowance',
+                    'Operating_cost', 'Total_asset', 'GDP', 'CPI'))
+print(df)
+
+# Count missing data
+missing_data = df.isna().sum()
+print(missing_data)
+
+# Insert some new columns
+df['NIM'] = df['Net_revenue']/df['Earning_asset']
+df['Credit_risk'] = (df['Credit_insti_allowance'] + df['Cust_allowance'])/(df['Credit_insti_lending'] + df['Cust_lending'])
+df['OCR'] = df['Operating_cost']/df['Total_asset']
+
+# Pivot table
+pivot_year = pd.pivot_table(df,
+                       columns=('Year'),
+                       values=('NIM'),
+                       aggfunc=np.mean)
+print(pivot_year)
+
+pivot_name = pd.pivot_table(df,
+                       columns=('Name'),
+                       values=('NIM'),
+                       aggfunc=np.mean)
+print(pivot_name)
+
+# Plot 1
+# Year_NIM
+# df_summary_year = df[['Year', 'NIM']].groupby('Year').mean()
+# x, y = list(df_summary_year.index), df_summary_year['NIM'].values
+# plt.figure(figsize=(16, 8))
+# plt.plot(x, y, marker='o', markersize=10, linestyle='-.', linewidth=2)
+# plt.xlabel('Year', fontsize=16)
+# plt.ylabel('%NIM', fontsize=16)
+# plt.title("Average of NIM by year", fontsize=18)
+# plt.show()
+
+# # CPI
+# df_summary_CPI= df[['Year', 'CPI']].groupby('Year').mean()
+# x1, y1 = list(df_summary_CPI.index), df_summary_CPI['CPI'].values
+# plt.figure(figsize=(16, 8))
+# plt.plot(x1, y1, marker='o', markersize=10, linestyle='-.', linewidth=2)
+# plt.xlabel('Year', fontsize=16)
+# plt.ylabel('%CPI', fontsize=16)
+# plt.title("CPI by year", fontsize=18)
+# plt.show()
+
+# # GDP
+# df_summary_GDP= df[['Year', 'GDP']].groupby('Year').mean()
+# x1, y1 = list(df_summary_GDP.index), df_summary_GDP['GDP'].values
+# plt.figure(figsize=(16, 8))
+# plt.plot(x1, y1, marker='o', markersize=10, linestyle='-.', linewidth=2)
+# plt.xlabel('Year', fontsize=16)
+# plt.ylabel('%GDP', fontsize=16)
+# plt.title("GDP by year", fontsize=18)
+# plt.show()
+
+# Plot 2
+# name_filter = ['Tech', 'VIB', 'VP', 'Vietcom', 'Vietin']
+
+# # Filter the DataFrame for multiple names
+# filtered_df = df[df['Name'].isin(name_filter)]
+
+# # Configure the plot
+# colors = ['red', 'blue', 'green', 'coral', 'brown']
+
+# # Create the line graph
+# fig, ax = plt.subplots()
+
+# for i, name in enumerate(name_filter):
+#     name_df = filtered_df[filtered_df['Name'] == name]
+#     years = name_df['Year'].tolist()
+#     nim_values = name_df['NIM'].tolist()
+#     ax.plot(years, nim_values, color=colors[i], label=name, marker='o', markersize=5, linewidth=2)
+
+# # Add labels and title
+# ax.set_xlabel('Year')
+# ax.set_ylabel('NIM %')
+# ax.set_title('NIM for top banks')
+
+# # Add legend
+# ax.legend()
+
+# # Display the chart
+# plt.tight_layout()
+# plt.show()
+
+# # Plot 3
+# name_filter = ['Tech', 'VIB', 'VP', 'Vietcom', 'Vietin']
+
+# # Filter the DataFrame for multiple names
+# filtered_df = df[df['Name'].isin(name_filter)]
+
+# # Configure the plot
+# colors = ['red', 'blue', 'green', 'coral', 'brown']
+
+# # Create the line graph
+# fig, ax = plt.subplots()
+
+# for i, name in enumerate(name_filter):
+#     name_df = filtered_df[filtered_df['Name'] == name]
+#     years = name_df['Year'].tolist()
+#     nim_values = name_df['NPL'].tolist()
+#     ax.plot(years, nim_values, color=colors[i], label=name, marker='o', markersize=5, linewidth=2)
+
+# # Add labels and title
+# ax.set_xlabel('Year')
+# ax.set_ylabel('NPL %')
+# ax.set_title('NPL for top banks')
+
+# # Add legend
+# ax.legend()
+
+# # Display the chart
+# plt.tight_layout()
+# plt.show()
+
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Banks_of_Vietnam_model_2012_2022
+
+
+Pivot table and chart result
+
+In general, there was an uptrend in the NIM indicator for over 10-year period
+A plunge was seen in the year of 2013 at about 0.023 and continued keeping a low rate in 2014. After that, the rate increased gradually. If compared with the chart of CPI by year, we can see a significant decrease from 2012 to 2015. There was likely the relationship between CPI and NIM. However, NIM indicator was recovered sooner than CPI indicator
+Factly, Vietnam implemented a stricter monetary policy in order to control price in the period of 2013-2015. 
+
+Top 5 greatest NIM banks are Techcombank, VIB, VPbank, Vietcombank and Vietinbank. Some highlights for the fluctuation of NIM over the period was observed following:
+
+Techcombank was seen in the uptrend and decrease gradually from 2017 to 2021 and decrease slightly in 2022
+VPbank experienced the highest increase from 2014 to 2019 and then plunged and recovered until 2022
+VIB was observed in the good state of NIM from 2016 to 2022
+Although Vietcombank and Vietinbank had a quite low rate of NIM compared other banks, it can not conclude that these banks were not operating well. Because these two banks are two banks which has greatest amount of earning assets. Net revenue only includes amount of interest from lending. For example, Vietcombank gets a large earning from stock investment every year. 
+
+Plot 3 section showed NPL of top banks during the period. In general, NPL ratio for all banks decreased. Especially in two countries including Techcombank and Vietcombank had both an significant decrease in this ratio. Techcombank completed well in the task of non-performing debts management from 2013 to 2020, and slightly increase after that. It was easily seen from the chart, VPbank had some issues with debt performance from 2020 to 2022. When seen in the chart of NIM, VPbank experienced a plunge in this period. 
+
+
+
+
